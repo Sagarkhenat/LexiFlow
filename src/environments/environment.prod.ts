56543079c0f1b46e05a87a8aa30456978616ac5c
unknown <sgkhenat94@gmail.com>
2026-03-02 14:11:10 +0530

    gitignore file updated

--- src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-  geminiApiKey: 'AIzaSyBxofSvMg3hrXTilGFS9Q18ROvD_0AOIz4',
-  geminiUrl: 'https://generativelanguage.googleapis.com/v1beta/models/gemini-3-flash-preview:generateContent'
-};
